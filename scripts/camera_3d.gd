bdc9c08db7f304a1815fda5855343e96829317a7
David Říha <davedave0008@gmail.com>
2025-11-28 22:19:52 +0100

    Added comments

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -4,6 +4,11 @@ const RAY_LENGTH = 1000
 var last_collider
 
 func _physics_process(delta):
+	raycast_forward_and_invoke_colliders()
+
+	
+func raycast_forward_and_invoke_colliders():
+		#Cast a ray and find the collider
 	var space_state = get_world_3d().direct_space_state
 	
 	var forward_vec = -self.transform.basis.z
@@ -14,7 +19,10 @@ func _physics_process(delta):
 	var result = space_state.intersect_ray(query)
 	
 	var colliding_obj = result.get("collider")
+	
+	#Check if the collider exists and whether it is the camera one
 	if colliding_obj and colliding_obj.is_in_group("camera_ray_collider_group"):
+		#If last collider exists -> swap it for the current one if they are different
 		if last_collider:
 			if last_collider == colliding_obj:
 				return
@@ -23,9 +31,9 @@ func _physics_process(delta):
 		colliding_obj.camera_ray_entered()
 		last_collider = colliding_obj
 	
+	#No collider found -> undo last collider
 	elif last_collider:
 		last_collider.camera_ray_left()
 		last_collider = null
 	
 	return
-	
