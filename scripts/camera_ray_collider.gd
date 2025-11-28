31e05673c65b7c7030ea9d858625e5476902545f
David Říha <davedave0008@gmail.com>
2025-11-28 22:07:21 +0100

    add E popup

--- scripts/camera_ray_collider.gd
+++ scripts/camera_ray_collider.gd
@@ -1,4 +1,15 @@
 extends Area3D
 
+func _ready() -> void:
+	#$SpriteLetterE.process_mode = Node.PROCESS_MODE_DISABLED
+	$SpriteLetterE.visible = false
+
 func camera_ray_entered():
-	print("Hellooooo")
+	#print("ray_entered")
+	#$SpriteLetterE.process_mode = Node.PROCESS_MODE_ALWAYS
+	$SpriteLetterE.visible = true
+
+func camera_ray_left():
+	#print("ray_left")
+	#$SpriteLetterE.process_mode = Node.NOTIFICATION_DISABLED
+	$SpriteLetterE.visible = false
