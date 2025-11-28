2cfd118309ba6033a43f4c5ad6f6a369309d303b
David Říha <davedave0008@gmail.com>
2025-11-28 22:44:14 +0100

    Changed inputMap and enabled interactables

--- scripts/camera_ray_collider.gd
+++ scripts/camera_ray_collider.gd
@@ -1,10 +1,23 @@
 extends Area3D
 
+var is_active: bool = false
+
 func _ready() -> void:
 	$SpriteLetterE.visible = false
+	
+func _process(delta: float) -> void:
+	if is_active:
+		if Input.is_action_just_pressed("Interact"):
+			interact()
 
 func camera_ray_entered():
 	$SpriteLetterE.visible = true
+	is_active = true
+	
 
 func camera_ray_left():
 	$SpriteLetterE.visible = false
+	is_active = false
+
+func interact():
+	print("Congratulations! You just read the book and lost 2 hours of your life. Hope you are balder next time")
