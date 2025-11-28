d6f7e76a03c18e330254ed2e04c98e724817fb5e
Daniel Martinek <martid12@fit.cvut.cz>
2025-11-28 23:16:21 +0100

    feat: Total stats for money, ...

--- global.gd
+++ global.gd
@@ -7,15 +7,27 @@ const LONG_PASS_TIME: int = 15
 var time_remaining: int = DEFAULT_TIME_LIMIT
 
 const SALARY_AMOUNT: int = 100
-const TIP_AMOUNTS: Array[int] = [15, 20, 25]
+const TIP_AMOUNTS: Array[int] = [5, 10, 15]
+const NASEL_MANZELKU_BONUSES: Array[int] = [15, 30, 40]
 const RENT_AMOUNT: int = 100
 const BILL_AMOUNTS: Array[int] = [0, 25, 50, 75, 100, 150, 200]
 const INITIAL_MONEY: int = 250
 var money: int = INITIAL_MONEY
+var todays_salary: int = 0
+var todays_tips: int = 0
+var todays_nasel_manzelku_bonuses_count: int = 0
 
+var current_day: int = 0
 
-func reset_time() -> void:
-    time_remaining = DEFAULT_TIME_LIMIT
+'''
+GAME LOOP:
+    1. Player chooses LEARN or GUESS - can't learn over 100%
+    2. Add SALARY and TIP based on choice (TIP only if you CAN'T know the answer)
+    3. Pass TIME based on choice
+    --- Go back to 1. until TIME runs out ---
+    4. Pay RENT and BILLS, get NASEL MANZELKU BONUS
+    5. Advance DAY -> Go to 1.
+'''
 
 
 func pass_time_short() -> void:
@@ -31,13 +43,27 @@ func add_money_salary() -> void:
 
 
 func add_money_tip() -> void:
-    var tip_index = randi() % TIP_AMOUNTS.size()
-    money += TIP_AMOUNTS[tip_index]
+    var tip = TIP_AMOUNTS.pick_random()
+    money += tip
+    todays_tips += tip
+
+
+func add_money_nasel_manzelku_bonus() -> void:
+    var bonus = NASEL_MANZELKU_BONUSES.pick_random()
+    money += bonus
 
 
 func pay_rent() -> void:
     money -= RENT_AMOUNT
 
 
-func pay_bill(bill_index: int) -> void:
-    money -= BILL_AMOUNTS[bill_index]
+func pay_bill() -> void:
+    money -= BILL_AMOUNTS[current_day]
+
+
+func next_day() -> void:
+    current_day += 1
+    time_remaining = DEFAULT_TIME_LIMIT
+    todays_salary = 0
+    todays_tips = 0
+    todays_nasel_manzelku_bonuses_count = 0
