67aeb8bb7a3c015b29ce84264ac65e7edd311735
David Říha <davedave0008@gmail.com>
2025-11-29 13:58:33 +0100

    Table textured

--- scripts/camera_ray_collider.gd
+++ scripts/camera_ray_collider.gd
@@ -4,11 +4,13 @@ extends Area3D
 @export var deafult_sprite: Texture2D
 @export var highlighted_sprite: Texture2D
 
+var table_sprite: Sprite3D
 
 
 var is_active: bool = false
 
 func _ready() -> void:
+	table_sprite = $InteractableSprite
 	$SpriteLetterE.visible = false
 	
 func _process(delta: float) -> void:
@@ -19,18 +21,18 @@ func _process(delta: float) -> void:
 func camera_ray_entered():
 	$SpriteLetterE.visible = true
 	is_active = true
-	$Sprite3D.texture = highlighted_sprite
+	table_sprite.texture = highlighted_sprite
 	
 	#testing purposes - remove
-	$Sprite3D.modulate = Color.BISQUE
+	table_sprite.modulate = Color.BISQUE
 
 func camera_ray_left():
 	$SpriteLetterE.visible = false
 	is_active = false
-	$Sprite3D.texture = highlighted_sprite
+	table_sprite.texture = highlighted_sprite
 	
 	#testing purposes - remove
-	$Sprite3D.modulate = Color.WHITE
+	table_sprite.modulate = Color.WHITE
 
 func interact():
 	if not displayed_text:
