33c7e74e5b0936f2b672e19c806529adf4862453
Daniel Martinek <martid12@fit.cvut.cz>
2025-11-29 21:15:56 +0100

    fix: Final clock object

--- scripts/clock.gd
+++ scripts/clock.gd
@@ -4,6 +4,8 @@ extends Node3D
 var hour_holder: Node3D
 var minute_holder: Node3D
 
+var hour_start_angle: float = 0
+var minute_start_angle: float = 0
 
 var local_minutes: float = 0
 
@@ -11,6 +13,8 @@ var local_minutes: float = 0
 func _ready() -> void:
     hour_holder = $Base/HourHolder
     minute_holder = $Base/MinuteHolder
+    hour_start_angle = hour_holder.rotation_degrees.z
+    minute_start_angle = minute_holder.rotation_degrees.z
 
 
 func _process(delta: float) -> void:
@@ -24,5 +28,5 @@ func update_clock(minutes: int) -> void:
     var minute_angle = (minutes % 60) * 6.0
     var hour_angle = (hours % 12) * 30.0 + (minutes % 60) * 0.5
 
-    minute_holder.rotation_degrees = Vector3(0, 0, -minute_angle)
-    hour_holder.rotation_degrees = Vector3(0, 0, -hour_angle)
+    minute_holder.rotation_degrees = Vector3(0, 0, minute_start_angle - minute_angle)
+    hour_holder.rotation_degrees = Vector3(0, 0, hour_start_angle - hour_angle)
